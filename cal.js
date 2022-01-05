231d2a95dff1a2a753130fc05903cdb68ecde2cc
hurayra10 <abuhurayrapust10@gmail.com>
2022-01-05 18:21:34 +0600

    Own calculator

---
+++ cal.js
@@ -0,0 +1,9 @@
+
+            var num1 = parseFloat(prompt("Enter your first number :"));
+            var num2 = parseFloat(prompt("Enter your second number :"));
+            var sum, sub;
+            sum = num1 + num2;
+            sub = num1 - num2;
+            document.write(sum);
+            document.write(sub);
+            // console.log(sum);
